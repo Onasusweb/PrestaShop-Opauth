ee0f96cee15b6b65c5430b06da1513c5b1b014da
Amaury PLANÇON <contact@amaury-plancon.com>
2012-12-04 14:44:41 +0100

    // Fix LinkedIn Bug + Create 1.5.x folder

---
+++ 1.5.x/opauthmodule/index.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Opauth example
+ * 
+ * This is an example on how to instantiate Opauth
+ * For this example, Opauth config is loaded from a separate file: opauth.conf.php
+ * 
+ */
+
+/**
+ * Define paths
+ */
+ 
+session_start();
+
+include(dirname(__FILE__).'/../../config/config.inc.php');
+include(dirname(__FILE__).'/../../init.php');
+include(dirname(__FILE__).'/opauthmodule.php');
+
+$opauth = new opauthmodule();
+
+define('CONF_FILE', dirname(__FILE__).'/'.'opauth.conf.php');
+define('OPAUTH_LIB_DIR', dirname(__FILE__).'/lib/Opauth/');
+
+/**
+* Load config
+*/
+
+if (!file_exists(CONF_FILE)){
+	trigger_error('Config file missing at '.CONF_FILE, E_USER_ERROR);
+	exit();
+}
+
+require CONF_FILE;
+
+/**
+ * Instantiate Opauth with the loaded config
+ */
+
+require OPAUTH_LIB_DIR.'Opauth.php';
+
+$Opauth = new Opauth( $config_op );
+
+?>
---
+++ 1.5.x/opauthmodule/notify.php
@@ -0,0 +1,15 @@
+<?php
+
+/* SSL Management */
+$useSSL = true;
+
+include(dirname(__FILE__).'/../../config/config.inc.php');
+include(dirname(__FILE__).'/../../header.php');
+
+$err=Tools::getValue('err');
+$ev = new Opauthmodule();
+
+echo $ev->notify($err);
+
+include_once(dirname(__FILE__).'/../../footer.php');
+?>
